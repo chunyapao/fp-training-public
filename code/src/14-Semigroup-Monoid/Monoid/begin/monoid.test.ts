b1f4f07d3dcfc1833f1656289ddad7600aa73b04
Chunya Paomuangchol <chunya@odds.team>
2026-05-02 13:51:29 +0700

    lab 14

--- code/src/14-Semigroup-Monoid/Monoid/begin/monoid.test.ts
+++ code/src/14-Semigroup-Monoid/Monoid/begin/monoid.test.ts
@@ -1,17 +1,47 @@
-describe('sum monoid', () => {
-  it('should sum an array of numbers', () => {
+export type Monoid<A> = Readonly<{
+  empty: A;
+  concat: (first: A, second: A) => A;
+}>;
 
-  })
+export type Sum<A> = (ns: A[]) => A;
 
-  it('should sum an array of strings', () => {
+export const sum =
+  <A>(M: Monoid<A>) =>
+  (ns: A[]): A =>
+    ns.length === 0 ? M.empty : M.concat(ns[0], sum(M)(ns.slice(1)));
 
-  })
+describe("sum monoid", () => {
+  it("should sum an array of numbers", () => {
+    const sumNumbers = sum({
+      empty: 0,
+      concat: (a: number, b: number) => a + b,
+    });
+    expect(sumNumbers([1, 1, 1, 1, 1])).toBe(5);
+  });
 
-  it('should sum an array of booleans', () => {
+  it("should sum an array of strings", () => {
+    const sumStrings = sum({
+      empty: "",
+      concat: (a: string, b: string) => a + b,
+    });
+    expect(sumStrings(["Hello", "World"])).toBe("HelloWorld");
+  });
 
-  })
+  it("should sum an array of booleans", () => {
+    const sumBooleans = sum({
+      empty: false,
+      concat: (a: boolean, b: boolean) => a || b,
+    });
+    expect(sumBooleans([true, false, true])).toBe(true);
+  });
 
-  it('should sum an array of array of string', () => {
-
-  })
-})
+  it("should sum an array of array of string", () => {
+    const sumArrayOfArray = sum({
+      empty: [],
+      concat: (a: string[], b: string[]) => a.concat(b),
+    });
+    expect(sumArrayOfArray([["Hello"], ["Functional", "Programming"]])).toEqual(
+      ["Hello", "Functional", "Programming"],
+    );
+  });
+});
