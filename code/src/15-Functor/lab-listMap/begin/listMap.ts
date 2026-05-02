c467eb258a3d317475613c67071cbbb9ae4b4c72
Chunya Paomuangchol <chunya@odds.team>
2026-05-02 13:59:56 +0700

    lab 15

--- code/src/15-Functor/lab-listMap/begin/listMap.ts
+++ code/src/15-Functor/lab-listMap/begin/listMap.ts
@@ -1,1 +1,18 @@
-// Start here...
+type Inc = (n: number[]) => number[];
+
+export const inc: Inc = (ns) =>
+  ns.length === 0 ? [] : [ns[0] + 1, ...inc(ns.slice(1))];
+
+type Sqr = (n: number[]) => number[];
+
+export const sqr: Sqr = (ns) =>
+  ns.length === 0 ? [] : [ns[0] * ns[0], ...sqr(ns.slice(1))];
+
+type ListMap = (f: (a: number) => number) => (as: number[]) => number[];
+
+export const listMap: ListMap = (f) => (as) =>
+  as.length === 0 ? [] : [f(as[0]), ...listMap(f)(as.slice(1))];
+
+type Map = <A, B>(f: (a: A) => B) => (as: A[]) => B[];
+export const map: Map = (f) => (as) =>
+  as.length === 0 ? [] : [f(as[0]), ...map(f)(as.slice(1))];
